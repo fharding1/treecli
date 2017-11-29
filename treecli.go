53e9c407d8f26657befe0e4755056c7c1ea459c2
Franklin Harding <franklinharding0.0@gmail.com>
2017-11-28 19:22:37 -0800

    Fixed documentation

--- treecli.go
+++ treecli.go
@@ -1,133 +1,95 @@
 package treecli
 
 import (
-  "fmt"
+	"fmt"
 )
 
-// TreeCLI ...
-// Root tree for your command tree.
-// Simply holds a root node for all
-// to stem off of.
+// TreeCLI holds the root node for all nodes to stem off.
 type TreeCLI struct {
-  root *Node
+	root *Node
 }
 
-// Node ...
-// Holds a command node with a function
-// that is returned when looking up a path,
-// and children nodes that are children of that
-// path. For example if this is path "auth",
-// children might be ["token", "user"] for the
-// total path "auth token" or "auth user".
+// Node holds a command node with a function that is returned from looking up a path,
+// for example if the path is "auth", children might be ["token", "user"] for the total
+// path "auth token" or "auth user".
 type Node struct {
-  f        func()
-  children map[string]*Node
+	f        func()
+	children map[string]*Node
 }
 
-// NewTree ...
-// Creates a new tree. The given func
-// should be the function to give if no params
-// are given. (help)
+// NewTree creates a new tree. The given function will be returned if no params are given
+// when parsing a path.
 func NewTree(f func()) *TreeCLI {
-  return &TreeCLI{&Node{f, make(map[string]*Node)}}
+	return &TreeCLI{&Node{f, make(map[string]*Node)}}
 }
 
-// AddCommand ...
-// Adds a command node to the CLI tree. Given
-// a function to associate w/ the specified
-// path.
+// AddCommand adds a command node to the tree, given a function to associate with the
+// specified path.
 func (ct *TreeCLI) AddCommand(f func(), path ...string) error {
-  cur := ct.root
-  for i, v := range path {
-    if v == "*" && i != len(path)-1 {
-      return fmt.Errorf("error: wildcards cannot have children")
-    } else if i == len(path)-1 && cur.children[v] == nil {
-      cur.children[v] = &Node{f, make(map[string]*Node)}
-    } else if _, ok := cur.children[v]; ok == true {
-      cur = cur.children[v]
-    } else {
-      return fmt.Errorf("error: does not contain node %s in path at position %d of %d", v, i, len(path)-1)
-    }
-  }
-  return nil
+	cur := ct.root
+	for i, v := range path {
+		if v == "*" && i != len(path)-1 {
+			return fmt.Errorf("error: wildcards cannot have children")
+		} else if i == len(path)-1 && cur.children[v] == nil {
+			cur.children[v] = &Node{f, make(map[string]*Node)}
+		} else if _, ok := cur.children[v]; ok == true {
+			cur = cur.children[v]
+		} else {
+			return fmt.Errorf("error: does not contain node %s in path at position %d of %d", v, i, len(path)-1)
+		}
+	}
+	return nil
 }
 
-// SimpleMessage ...
-// This is a helper function for making a command
-// node that simply says a message, for example for
-// help messages.
-func (ct *TreeCLI) SimpleMessage(msg string, path ...string) error {
-  cur := ct.root
-  for i, v := range path {
-    if v == "*" && i != len(path)-1 {
-      return fmt.Errorf("error: wildcards cannot have children")
-    } else if i == len(path)-1 && cur.children[v] == nil {
-      cur.children[v] = &Node{func() {
-        fmt.Println(msg)
-      }, make(map[string]*Node)}
-    } else if _, ok := cur.children[v]; ok == true {
-      cur = cur.children[v]
-    } else {
-      return fmt.Errorf("error: does not contain node %s in path at position %d of %d", v, i, len(path)-1)
-    }
-  }
-  return nil
-}
-
-// RemoveCommand ...
-// Removes a command from the CLI tree. Given
-// the path to that command node.
+// RemoveCommand removes a command from the tree, given a path to that node.
 func (ct *TreeCLI) RemoveCommand(path ...string) error {
-  n, k, err := ct.searchTree(path...)
-  if err != nil {
-    return err
-  }
-  n.children[k] = nil
-  return nil
+	n, k, err := ct.searchTree(path...)
+	if err != nil {
+		return err
+	}
+	n.children[k] = nil
+	return nil
 }
 
-// String ...
-// Kinda pretty prints the CLI tree recursively.
+// String pretty prints the tree.
 func (ct *TreeCLI) String() (str string) {
-  var printChildren func(*Node, int)
-  printChildren = func(n *Node, indent int) {
-    for k, v := range n.children {
-      str += fmt.Sprintf("%*s%s\n", indent, "", k)
-      printChildren(v, indent+2)
-    }
-  }
-  printChildren(ct.root, 0)
-  return str
+	var printChildren func(*Node, int)
+	printChildren = func(n *Node, indent int) {
+		for k, v := range n.children {
+			str += fmt.Sprintf("%*s%s\n", indent, "", k)
+			printChildren(v, indent+2)
+		}
+	}
+	printChildren(ct.root, 0)
+	return str
 }
 
-// Parse ...
-// Parses a path (variadic strings) and returns
-// the function present from that path.
+// Parse parses a path (variadic strings) and returns the function at that node.
 func (ct *TreeCLI) Parse(path ...string) (func(), error) {
-  if len(path) == 0 {
-    return ct.root.f, nil
-  }
-  n, k, err := ct.searchTree(path...)
-  if err != nil {
-    return nil, err
-  }
-  return n.children[k].f, nil
+	if len(path) == 0 {
+		return ct.root.f, nil
+	}
+	n, k, err := ct.searchTree(path...)
+	if err != nil {
+		return nil, err
+	}
+	return n.children[k].f, nil
 }
 
 func (ct *TreeCLI) searchTree(path ...string) (*Node, string, error) {
-  cur := ct.root
-  for i, v := range path {
-    if cur.children["*"] != nil {
-      return cur, "*", nil
-    } else if i == len(path)-1 && cur.children[v] != nil {
-      return cur, v, nil
-    } else if i == len(path)-1 && cur.children[v] == nil {
-      return nil, "", fmt.Errorf("error: that node does not exist")
-    } else if _, ok := cur.children[v]; ok == true {
-      cur = cur.children[v]
-    } else {
-      return nil, "", fmt.Errorf("error: does not contain node %s where path does", v)
-    }
-  }
-  return nil, "", fmt.Errorf("error: tree does not contain node in path")
+	cur := ct.root
+	for i, v := range path {
+		if cur.children["*"] != nil {
+			return cur, "*", nil
+		} else if i == len(path)-1 && cur.children[v] != nil {
+			return cur, v, nil
+		} else if i == len(path)-1 && cur.children[v] == nil {
+			return nil, "", fmt.Errorf("error: that node does not exist")
+		} else if _, ok := cur.children[v]; ok == true {
+			cur = cur.children[v]
+		} else {
+			return nil, "", fmt.Errorf("error: does not contain node %s where path does", v)
+		}
+	}
+	return nil, "", fmt.Errorf("error: tree does not contain node in path")
 }
